bfedd3a6f956f810646443ef5e334c5ccb166cb2
“Yashwanth142” <yashwanthvishnu14@gmail.com>
2023-01-12 13:57:03 +0530

    Welcome

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace LinkedListDemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            LinkedList list= new LinkedList();
+            list.Add(10);
+            list.Add(30);
+            list.Add(60);
+            list.Add(90);
+
+            list.Display();
+        }
+    }
+}
