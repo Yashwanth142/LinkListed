d01fb407a09956d089a02211c4f2d7d28d7e47ab
“Yashwanth142” <yashwanthvishnu14@gmail.com>
2023-01-12 14:12:59 +0530

    Linked list insert between added

--- Program.cs
+++ Program.cs
@@ -6,11 +6,15 @@
         {
             LinkedList list = new LinkedList();
 
-            Console.WriteLine("Current Size of the list is: " + list.getListSize());
+            Console.WriteLine("Current Size of the List is: " + list.getListSize());
 
             list.insertNode(56);
-            list.insertNode(30);
+
             list.insertNode(70);
+            list.printNodes();
+
+            Console.WriteLine("Adding 30 at 1st Location");
+            list.insertNth(30, 1);
             list.printNodes();
         }
     }
